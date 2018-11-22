6f75c279d19fe740816bd8edf26f90e9febaeae1
CEKTAHT <n.dremkoff@gmail.com>
2018-11-22 22:25:49 +0200

    9. Clear Errors upon Registration, Show Loading State

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -18,6 +18,7 @@ class Register extends React.Component {
 		password: '',
 		passwordConfirmation: '',
 		errors: [],
+		loading: false,
 	};
 
 	isFormValid = () => {
@@ -64,18 +65,30 @@ class Register extends React.Component {
 	};
 
 	handleSubmit = event => {
+		event.preventDefault();
 		if (this.isFormValid()) {
-			event.preventDefault();
+			this.setState({ errors: [], loading: true });
 			firebase
 				.auth()
 				.createUserWithEmailAndPassword(this.state.email, this.state.password)
 				.then(createdUser => {
 					console.log(createdUser);
+					this.setState({ loading: false });
 				})
 				.catch(err => {
 					console.error(err);
+					this.setState({
+						errors: this.state.errors.concat(err),
+						loading: false,
+					});
 				});
 		}
+	};
+
+	handleInputError = (errors, inputName) => {
+		return errors.some(error => error.message.toLowerCase().includes(inputName))
+			? 'error'
+			: '';
 	};
 
 	render() {
@@ -85,6 +98,7 @@ class Register extends React.Component {
 			password,
 			passwordConfirmation,
 			errors,
+			loading,
 		} = this.state;
 		return (
 			<Grid textAlign="center" verticalAlign="middle" className="app">
@@ -113,6 +127,7 @@ class Register extends React.Component {
 								placeholder="Email Address"
 								onChange={this.handleChange}
 								value={email}
+								className={this.handleInputError(errors, 'email')}
 								type="email"
 							/>
 							<Form.Input
@@ -123,6 +138,7 @@ class Register extends React.Component {
 								placeholder="Password"
 								onChange={this.handleChange}
 								value={password}
+								className={this.handleInputError(errors, 'password')}
 								type="password"
 							/>
 							<Form.Input
@@ -133,9 +149,16 @@ class Register extends React.Component {
 								placeholder="Password Confirmation"
 								onChange={this.handleChange}
 								value={passwordConfirmation}
+								className={this.handleInputError(errors, 'password')}
 								type="password"
 							/>
-							<Button color="orange" fluid size="large">
+							<Button
+								disabled={loading}
+								className={loading ? 'loading' : ''}
+								color="orange"
+								fluid
+								size="large"
+							>
 								Submit
 							</Button>
 						</Segment>
